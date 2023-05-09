e7a1616950f4f7ae7e2be55f795d7491fb31450b
felixDev22 <felixnzomo@gmail.com>
2023-05-10 00:50:13 +0300

    Create the App component with react-route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useDispatch } from 'react-redux';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
+import { FetchGreeting } from './Redux/greetingSlice';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(FetchGreeting());
+  }, [dispatch]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
     </div>
   );
 }
